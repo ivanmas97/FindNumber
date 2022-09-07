f9603709ddcec97165aad98955024c3f3d2724fa
ivanmas97 <ivanmas97@yandex.ru>
2022-09-07 19:10:29 +0300

    Pre release 0.2
    Added timer, mistake counter, "New game" button. Started making constraints to support other devices.

--- Game.swift
+++ Game.swift
@@ -10,7 +10,8 @@ import UIKit
 
 enum StatusGame {
     case Start
-    case win
+    case Win
+    case Loose
 }
 
 
@@ -18,7 +19,8 @@ class Game{
     
     struct Item{
         var title: String
-        var isFound: Bool = false
+        var isFound = false
+        var isError = false
         
     }
     
@@ -30,35 +32,91 @@ class Game{
     
     var nextItem: Item?
     
-    var status: StatusGame = .Start
+    var status: StatusGame = .Start {
+        didSet {
+            if status != .Start {
+                stopGame()
+            }
+        }
+    }
     
-    init(countItems: Int) {
+    private var secondsGame: Int {
+        didSet {
+            if secondsGame == 0 {
+                status = .Loose
+            }
+            updateTimer(status, secondsGame)
+        }
+    }
+    
+    private var timer: Timer?
+    
+    private var timeForGame: Int
+    
+    var mistakes: Int
+    
+    private var updateTimer:((StatusGame, Int) -> Void)
+    
+    init(countItems: Int, time: Int, mistakes: Int, updateTimer:@escaping (_ status: StatusGame, _ seconds: Int) -> Void) {
         self.countItems = countItems
+        self.timeForGame = time
+        self.secondsGame = time
+        self.updateTimer = updateTimer
+        self.mistakes = 0
         setupGame()
     }
     
     private func setupGame() {
         var digits = data.shuffled()
+        items.removeAll()
         
         while items.count < countItems {
             let item = Item(title: String(digits.removeFirst()))
             items.append(item)
-            
         }
         
         nextItem = items.shuffled().first
+        
+        updateTimer(status, secondsGame)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self](_) in
+            self?.secondsGame -= 1
+        })
+    }
+    
+    func newGame() {
+        status = .Start
+        self.secondsGame = self.timeForGame
+        
+        setupGame()
     }
     
     func check(index: Int) {
+        guard status == .Start else {return}
         if items[index].title == nextItem?.title {
             items[index].isFound = true
             nextItem = items.shuffled().first(where: { (item) -> Bool in
                 item.isFound == false
             })
+        } else {
+            items[index].isError = true
+            mistakes += 1
         }
         
         if nextItem == nil {
-            status = .win
+            status = .Win
         }
+    }
+    
+    private func stopGame() {
+        timer?.invalidate()
+    }
+}
+
+extension Int {
+    func secondsToString() -> String {
+        let minutes = self / 60
+        let seconds = self % 60
+        return String(format: "%d:%02d", minutes, seconds)
     }
 }
--- GameViewController.swift
+++ GameViewController.swift
@@ -10,20 +10,26 @@ import UIKit
 class GameViewController: UIViewController {
     
     @IBOutlet var buttons: [UIButton]!
-    
     @IBOutlet weak var nextDigit: UILabel!
-    
     @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var mistakeLabel: UILabel!
+    @IBOutlet weak var newGameButton: UIButton!
     
-    lazy var game = Game(countItems: buttons.count)
-    
+    lazy var game = Game(countItems: buttons.count, time: 10, mistakes: 0) { [weak self] status, seconds in
+        
+        guard let self = self else {return}
+        
+        self.timerLabel.text = "Time left: \(seconds.secondsToString())"
+        self.updateGameInfo(with: status)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupScreen()
-        
     }
+    
     @IBAction func pressButton(_ sender: UIButton) {
         guard let buttonIndex = buttons.firstIndex(of: sender) else {return}
         game.check(index:buttonIndex)
@@ -31,25 +37,67 @@ class GameViewController: UIViewController {
         updateUI()
     }
     
-    private func setupScreen() {
+    @IBAction func newGame(_ sender: UIButton) {
+        game.newGame()
+        sender.isHidden == true
+        setupScreen()
+    }
     
+    private func setupScreen() {
+        
         for index in game.items.indices {
             buttons[index].setTitle(game.items[index].title, for: .normal)
             buttons[index].isHidden = false
         }
         
         nextDigit.text = game.nextItem?.title
+        
+        mistakeLabel.text = "0 mistakes"
     }
     
     private func updateUI() {
         for index in game.items.indices {
-            buttons[index].isHidden = game.items[index].isFound
+            
+            if game.items[index].isFound {
+                UIView.animate(withDuration: 0.3) { [weak self] in
+                    self?.buttons[index].backgroundColor = .systemGreen
+                } completion: { (_) in
+                    self.buttons[index].alpha = self.game.items[index].isFound ? 0 : 1
+                    self.buttons[index].isEnabled = !self.game.items[index].isFound
+                }
+            }
+            
+            if game.items[index].isError {
+                mistakeLabel.text = "\(game.mistakes) mistakes"
+                
+                UIView.animate(withDuration: 0.3) { [weak self] in
+                    self?.buttons[index].backgroundColor = .red
+                } completion: { [weak self] (_) in
+                    self?.buttons[index].backgroundColor = .white
+                    self?.game.items[index].isError = false
+                }
+            }
         }
         
         nextDigit.text = game.nextItem?.title
-
-        if game.status == .win {
+        
+        updateGameInfo(with: game.status)
+    }
+    
+    private func updateGameInfo(with status: StatusGame) {
+        switch status {
+        case .Start:
+            statusLabel.text = "Game started"
+            statusLabel.textColor = .black
+            newGameButton.isHidden = true
+        case .Win:
             statusLabel.text = "You win!"
+            statusLabel.textColor = .systemGreen
+            newGameButton.isHidden = false
+        case .Loose:
+            statusLabel.text = "You loose..."
+            statusLabel.textColor = .systemRed
+            newGameButton.isHidden = false
         }
     }
 }
